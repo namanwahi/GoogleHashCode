729902192e4b344870a67517ff997be6e5e809ba
namanwahi <namanwahi@yahoo.co.uk>
2018-03-01 20:43:30 +0000

    time to add scheduling algorithm

--- naman_hash_code_testing.py
+++ naman_hash_code_testing.py
@@ -44,7 +44,6 @@ for i in range(ride_count):
 def earliest_start_key(ride):
     return ride.earliest_start
 
-print("****************** SORTED *********************")
 rides.sort(key=earliest_start_key)
 
 for i in range(ride_count):
@@ -54,13 +53,45 @@ class Vehicle():
 
     def __init__(self, id):
         self.id = id
+        self.rides = []
 
-    def get_time_of_jounrey(self):
-        res = 0
+    def add_ride(self, ride):
+        self.rides.append(ride)
+
+    def get_score_of_jounrey(self):
+        time = 0
         pos = (0,0)
+        score = 0
         
-        for ride in rides:
-            res += distance_comput(pos, ride.start_point)
-            res += distance_comput(ride.start_point, ride.end_point)
+        for ride in self.rides:
+            ride_score = 0
+            
+            #travel to start of the ride
+            time += distance_comput(pos, ride.start_point)
+
+            #wait until the earliest time to go 
+            if (time <= ride.earliest_start):
+                time = ride.earliest_start
+                ride_score += bonus
+
+            #travel the distance
+            time += distance_comput(ride.start_point, ride.end_point)
+
+            #if arrived on time
+            if (time <= ride.latest_finish):
+                ride_score += distance_comput(ride.start_point, ride.end_point)
+
+            #if you have run out of time
+            if (time > steps):
+                break
+
+            #otherwise update the score
+            score += ride_score
+            
+            #set the final position after travelling
             pos = ride.end_point
-    return res
+    
+        return score
+
+
+
